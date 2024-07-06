c409379f9f3db412716f64e202942d2bed5b13a2
jpablopachar <jpablopachar1993@gmail.com>
2024-07-06 12:52:38 -0500

    Vista Password

--- react-client/src/pages/CheckEmail.jsx
+++ react-client/src/pages/CheckEmail.jsx
@@ -1,6 +1,77 @@
+import axios from 'axios'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { PiUserCircle } from 'react-icons/pi'
+import { Link, useNavigate } from 'react-router-dom'
+
 const CheckEmail = () => {
+  const [data, setData] = useState({ email: '' })
+
+  const navigate = useNavigate()
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      }
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const URL = `${import.meta.env.VITE_SERVER_URL}/api/email`
+
+    try {
+      const res = await axios.post(URL, data)
+
+      toast.success(res?.data?.message)
+
+      if (res.data.success) {
+        setData({ email: '' })
+
+        navigate('/password', { state: res?.data?.message })
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+    }
+  }
   return (
-    <div>CheckEmail</div>
+    <div className="mt-5">
+      <div className="bg-white w-full max-w-md rounded overflow-hidden p-4 mx-auto">
+        <div className="w-fit mx-auto mb-2">
+          <PiUserCircle size={80} />
+        </div>
+        <h3>Welcome to Chat App!</h3>
+        <form className="grid gap-4 mt-3" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              name="email"
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+          <button className="bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
+            Lets Go
+          </button>
+        </form>
+        <p className="my-3 text-center">
+          New User ?{' '}
+          <Link to={'/register'} className="hover:text-primary font-semibold">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
--- react-client/src/pages/CheckPassword.jsx
+++ react-client/src/pages/CheckPassword.jsx
@@ -1,6 +1,108 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+// import { useDispatch } from 'react-redux'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+
 const CheckPassword = () => {
+  const [data, setData] = useState({ password: '', userId: '' })
+
+  const navigate = useNavigate()
+  const location = useLocation()
+  // const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!location?.state?.name) navigate('/email')
+  }, [])
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      }
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const URL = `${import.meta.env.VITE_SERVER_URL}/api/password`
+
+    try {
+      const res = await axios({
+        method: 'post',
+        url: URL,
+        data: {
+          userId: location?.state?._id,
+          password: data.password,
+        },
+        withCredentials: true,
+      })
+
+      toast.success(res?.data?.message)
+
+      if (res.data.success) {
+        // dispatch(setToken)
+        localStorage.setItem('token', res?.data?.token)
+
+        setData({ password: '' })
+
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+    }
+  }
+
   return (
-    <div>CheckPassword</div>
+    <div className="mt-5">
+      <div className="bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto">
+        <div className="w-fit mx-auto mb-2 flex justify-center items-center flex-col">
+          {/* <PiUserCircle
+                  size={80}
+                /> */}
+          <Avatar
+            width={70}
+            height={70}
+            name={location?.state?.name}
+            imageUrl={location?.state?.profile_pic}
+          />
+          <h2 className="font-semibold text-lg mt-1">
+            {location?.state?.name}
+          </h2>
+        </div>
+        <form className="grid gap-4 mt-3" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password :</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="enter your password"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.password}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+          <button className="bg-primary text-lg  px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
+            Login
+          </button>
+        </form>
+        <p className="my-3 text-center">
+          <Link
+            to={'/forgot-password'}
+            className="hover:text-primary font-semibold"
+          >
+            Forgot password ?
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
